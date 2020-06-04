6619678570ea1b893bcc27fb54cd37f9ed402604
Ishan Ediriweera <reshmikaediriweera1997@gmail.com>
2020-06-04 22:03:43 +0530

    Variable indroduction other program

---
+++ variables1.java
@@ -0,0 +1,16 @@
+///variable same of the other programming language
+public class variables1{
+    public static void main(String[] args)
+    {
+       
+
+        double salary=20000.43;
+        salary=60000.00;
+        System.out.println("Your now salary is "+salary);//output should be 60000.00
+        
+        
+    }
+    
+}
+
+        
