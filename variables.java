7b5b1bbd2c152f93b6fb3c0fd1af0e9a5fc5b4bb
Ishan Ediriweera <reshmikaediriweera1997@gmail.com>
2020-06-04 22:03:07 +0530

    Variable indroduction

---
+++ variables.java
@@ -0,0 +1,17 @@
+///variable same of the other programming language
+public class variables{
+    public static void main(String[] args)
+    {
+        int age=20;
+        String name="ishan reshmika";
+        Boolean isMarried=false;
+        char gender='m';
+        double salary=20000.50;
+        System.out.println("My name is: "+name);
+        System.out.println("My age is: "+age);
+        System.out.println("I have to find "+salary);
+        System.out.println("Did you married "+isMarried);
+        
+    }
+    
+}
